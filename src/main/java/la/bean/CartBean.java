0826f1a875ab731bfd9219e047e049f0ef55ea3a
2022orizon-student013 <2022orizon-student013@la-study.com>
2022-05-24 17:08:03 +0900

    2022/05/24 17:07

---
+++ src/main/java/la/bean/CartBean.java
@@ -0,0 +1,21 @@
+package la.bean;
+
+import java.util.List;
+
+public class CartBean {
+
+	public CartBean() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public int getTotal() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+	public List<ItemBean> getItems() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
---
+++ src/main/java/la/bean/ItemBean.java
@@ -0,0 +1,15 @@
+package la.bean;
+
+public class ItemBean {
+
+	public int getCode() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+	public int getQuantity() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+}
